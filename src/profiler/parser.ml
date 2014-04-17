e44cf0d199d2ba4ec46d600f6bde86aeb6408ef8
Pravin Gohite <pgohite@users.noreply.github.com>
2014-04-17 01:31:06 -0400

    Update parser.ml

--- src/profiler/parser.ml
+++ src/profiler/parser.ml
@@ -1,20 +1,21 @@
 (* parser.ml *)
 (* Parser for profile data*)
-(* 
 (* Pravin Gohite *)
 
 
 type ftype = Enter | Exit
 
 let parse_profile_data filename =
-	let ic = open_in filename in
-	let rec parse in_ch =
-	  try
-      let line = input_line in_ch in 
-      print_endline line;
-		  flush stdout;
-			parse in_ch
-    with End_of_file ->
-      close_in in_ch
-	in
-	parse ic
+  let ic = open_in filename in
+  let rec parse in_ch =
+  try
+    let line = input_line in_ch in 
+    print_endline line;
+    flush stdout;
+    parse in_ch
+  
+  with End_of_file ->
+    close_in in_ch
+  in
+  
+  parse ic
